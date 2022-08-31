5e8d7fe06bf720e4248abe818364c33b8c6b0076
Hom Chen <homchen@Homde-MacBook-Pro.local>
2022-08-31 11:49:08 +0800

    切分 components

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/todo-list/',
-  plugins: [vue()],
-  test: {
-    globals: true,
-    setupFiles: 'src/setupTest.ts',
-    include: ['**/*.test.ts'],
-    environment: 'jsdom'
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const plugins = { plugins: [vue()] }
+  switch (mode) {
+    case 'production':
+      plugins.plugins.push(splitVendorChunkPlugin())
+      return Object.assign(plugins, { base: '/todo-list/', })
+    case 'test':
+      return Object.assign(plugins, {
+        test: {
+          globals: true,
+          setupFiles: 'src/setupTest.ts',
+          include: ['**/*.test.ts'],
+          environment: 'jsdom'
+        }
+      })
+    default:
+      return Object.assign(plugins, {})
   }
 })
